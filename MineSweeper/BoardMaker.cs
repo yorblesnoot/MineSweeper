17a8e648632c6b5c3441b583e09f994f1a65cc09
AcademyPgh <AcademyPgh@DESKTOP-KPU01Q0>
2023-04-27 09:56:13 -0400

    Add project files.

---
+++ MineSweeper/BoardMaker.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    internal class BoardMaker
+    {
+        public void RenderBoard(int[,] knownGrid, int highlightX, int highlightY)
+        {
+            //spacer character for use between grid elements
+            string spacer = "  ";
+
+            int boardSize = knownGrid.GetLength(0);
+
+            //greetings and instructions
+            Console.WriteLine("~~~~~~~~Minesweeper~~~~~~~~");
+            Console.WriteLine("W A S D: Move the cursor. C: Flag a square. V: Reveal a square.");
+
+            //generate text lines for printing on the screen
+
+            for (int x = 0; x < boardSize; x++)
+            {
+                for (int y = 0; y < boardSize-1; y++)
+                {
+                    //build out each grid line from the known grid values and spacers
+                    Console.Write(spacer);
+                    //if the currently rendering square is highlighted, change foreground and background for that square
+                    if (x == highlightX && y == highlightY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;                     
+                    }
+                    Console.Write(TranslateKnown(knownGrid[x, y]));
+                    //then change back to default
+                    Console.ResetColor();
+                    
+                }
+                Console.Write(spacer);
+                //to avoid misaligning the board lines, the last character of each row has to be a writeline
+                //theres probably a better way to do this lol
+                if (x == highlightX && boardSize - 1 == highlightY)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(TranslateKnown(knownGrid[x, boardSize - 1]));
+                Console.ResetColor();
+            }
+        }
+
+        public bool[,] GenerateHidden(int size, int frequency)
+        {
+            bool[,] boardNew = new bool[size, size];
+            //initialize the board with random mine placements, default visible tiles
+            Random rng = new Random();
+
+            //use rng to generate the minefield
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (rng.Next(frequency + 1) == frequency)
+                    {
+                        boardNew[x, y] = true;
+                    }
+                    else
+                    {
+                        boardNew[x, y] = false;
+                    }
+                }
+            }
+            return boardNew;
+        }
+
+        public int[,] GenerateKnown(int size)
+        {
+            int[,] boardNew = new int[size, size];
+
+            //Board states: -3 mine (game over), -2 unknown, -1 flagged, 0+ revealed
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    boardNew[x, y] = -2;
+                }
+            }
+            return boardNew;
+        }
+
+        public string TranslateKnown (int value)
+        {
+            //convert from int board values to string display values
+            string output = "";
+            if (value == -3)
+            {
+                //mine
+                output = "X";
+            }
+            else if(value == -2)
+            {
+                //unknown
+                output = "~";
+            }
+            else if (value == -1)
+            {
+                //flagged
+                output = "!";
+
+            }
+            else if (value >= 0)
+            {
+                //revealed
+                output = value.ToString();
+            }
+
+            return output;
+        }
+
+    }
+}
---
+++ MineSweeper/Program.cs
@@ -0,0 +1,209 @@
+﻿
+using MineSweeper;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Numerics;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace MineSweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //board generation parameters
+            int mineFrequency = 6;
+            int boardSize = 10;
+            
+
+            //generate boards
+            BoardMaker boardMaker = new BoardMaker();
+            bool[,] hiddenBoard = boardMaker.GenerateHidden(boardSize, mineFrequency);
+            int[,] knownBoard = boardMaker.GenerateKnown(boardSize);
+
+            int xHighlight = 0;
+            int yHighlight = 0;
+
+            bool gameOver = false;
+            bool youLose = true;
+
+            boardMaker.RenderBoard(knownBoard, xHighlight, yHighlight);
+            while (gameOver == false)
+            {
+                ConsoleKey keyPress = Console.ReadKey().Key;
+                switch (keyPress)
+                { 
+                    //mine    -3
+                    //unknown -2
+                    //flagged -1
+                    //revealed 0+
+
+                    case (ConsoleKey.W):
+                        if (xHighlight > 0)
+                        {
+                            //go up
+                            xHighlight -= 1;
+                        }
+                        break;
+
+                    case (ConsoleKey.A):
+                        if (yHighlight > 0)
+                        {
+                            //go left
+                            yHighlight -= 1;
+                        }
+                        break;
+
+                    case (ConsoleKey.D):
+                        if (yHighlight < boardSize - 1)
+                        {
+                            //go right
+                            yHighlight += 1;
+                        }
+                        break;
+
+                    case (ConsoleKey.S):
+                        if (xHighlight < boardSize - 1)
+                        {
+                            //go down
+                            xHighlight += 1;
+                        }
+                        break;
+
+                    case (ConsoleKey.C):
+                        //flag square
+                        if (knownBoard[xHighlight, yHighlight] == -2)
+                        {
+                            knownBoard[xHighlight, yHighlight] = -1;
+                        }
+                        else if(knownBoard[xHighlight, yHighlight] == -1)
+                        {
+                            knownBoard[xHighlight, yHighlight] = -2;
+                        }
+                        break;
+
+                    case (ConsoleKey.V):
+                        //pop square
+                        knownBoard = PopSquare(xHighlight, yHighlight, hiddenBoard, knownBoard);
+                        //if the square we popped was a mine, game over
+                        if (hiddenBoard[xHighlight,yHighlight] == true)
+                        {
+                            gameOver = true;
+                            youLose = true;
+                        }
+                        break;
+                }
+                //update the known board
+                Console.Clear();
+                boardMaker.RenderBoard(knownBoard, xHighlight, yHighlight);
+                //end the game if we revealed everything
+                if(CheckWin(knownBoard, hiddenBoard) == true)
+                {
+                    gameOver = true;
+                    youLose = false;
+                }
+            }
+            if (youLose == true)
+            {
+                Console.WriteLine("You hit a mine! GAME OVER.");
+            }
+            else if(youLose == false)
+            {
+                Console.WriteLine("You revealed all the tiles without hitting any mines. YOU WIN!");
+            }
+        }
+
+        public static int[,] PopSquare(int x, int y, bool[,] hiddenBoard, int[,] knownBoard)
+        {
+            int[,] gridUpdate = knownBoard;
+            int size = gridUpdate.GetLength(0);
+            //check that we didnt hit a mine
+           
+            if (hiddenBoard[x, y] == false)
+            {
+                //check the surrounding squares for mines and count them 
+                int bombCount = 0;
+                for (int xCheck = -1; xCheck < 2; xCheck++)
+                {
+                    for (int yCheck = -1; yCheck < 2; yCheck++)
+                    {
+                        int xtoCheck = x + xCheck;
+                        int ytoCheck = y + yCheck;
+                        if (xtoCheck >= 0 && xtoCheck < size && ytoCheck >= 0 && ytoCheck < size)
+                        {
+                            if (hiddenBoard[xtoCheck, ytoCheck] == true)
+                            {
+                                bombCount++;
+                            }
+                        }
+                    }
+                }
+                //reveal the number of mines around the chosen square
+                knownBoard[x,y] = bombCount;
+                //if we didnt find any mines, chain reveal adjacent squares
+                if (bombCount == 0)
+                {
+                    for (int xCheck = -1; xCheck < 2; xCheck++)
+                    {
+                        for (int yCheck = -1; yCheck < 2; yCheck++)
+                        {
+                            int xtoCheck = x + xCheck;
+                            int ytoCheck = y + yCheck;
+                            if (xtoCheck >= 0 && xtoCheck < size && ytoCheck >= 0 && ytoCheck < size)
+                            {
+                                //chain reveal on every adjacent square
+                                if (hiddenBoard[xtoCheck, ytoCheck] == false )
+                                {
+                                    //...that hasn't already been revealed
+                                    if (knownBoard[xtoCheck, ytoCheck] < 0)
+                                    {
+                                        //recur the function
+                                        gridUpdate = PopSquare(xtoCheck, ytoCheck, hiddenBoard, gridUpdate);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+            else 
+            {
+                //we hit a mine
+                gridUpdate[x, y] = -3;
+            }
+            return gridUpdate;
+        }
+
+        public static bool CheckWin(int[,] knownBoard, bool[,] hiddenBoard)
+        {
+            //check if the whole board is revealed
+            int revealCount = 0;
+            int mineCount = 0;
+            int totalCount = 0;
+            for (int xCheck = 0; xCheck < knownBoard.GetLength(0); xCheck++)
+            {
+                for (int yCheck = 0; yCheck < knownBoard.GetLength(1); yCheck++)
+                {
+                    if(hiddenBoard[xCheck,yCheck] == true)
+                    {
+                        mineCount++;
+                    }
+                    if (knownBoard[xCheck, yCheck] >= 0)
+                    {
+                        revealCount++;
+                    }
+                    totalCount++;
+                }
+            }
+            if(totalCount == mineCount + revealCount)
+            {
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+    }
+}
